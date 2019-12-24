a49df2f56105fe4836dde8ee6063a37783189d21
Ju Dantas <jdantas@thoughtworks.com>
2019-12-24 05:19:44 -0300

    Inserido mais testes:

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,7 +7,7 @@ const addViolation = (state, violationId) => {
   const violations = []
   violations.push(violationId)
   state.operationsHistoric.push({
-    account: { ...state.account},
+    account: { ...state.account },
     violations
   })
 
@@ -15,16 +15,17 @@ const addViolation = (state, violationId) => {
 }
 
 const isEmpty = object => {
-  for(const key in object) {
-      if(object.hasOwnProperty(key))
-          return false
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      return false
+    }
   }
   return true
 }
 
 const getOperation = operation => Object.keys(operation).shift()
 
-const groupBy = (array, callback ) => {
+const groupBy = (array, callback) => {
   return array.reduce((acc, item) => {
     var list = callback(item)
     acc[list] = acc[list] || []
@@ -53,7 +54,7 @@ const findDoubleTransaction = (operations, operation) => {
   return filtered.length > 1 ? true : false
 }
 
-const findOperation = (transactions, operation,) => {
+const findOperation = (transactions, operation, ) => {
   let label
   let time = getTime(operation.transaction.time)
 
--- tests/unit/utils/index.test.js
+++ tests/unit/utils/index.test.js
@@ -1,6 +1,31 @@
-const { createOperationList, findOperation } = require('../../../src/utils')
+const {
+  createOperationList,
+  addViolation,
+  getOperation,
+  findOperation,
+  isEmpty,
+  groupBy,
+  byDate,
+  findDoubleTransaction
+} = require('../../../src/utils')
 
 describe('Utils unit tests', () => {
+
+  it('Should return true if the value is empty', () => {
+    const test1 = "";
+    const result = isEmpty(test1)
+    const test2 = "s"
+    const result2 = isEmpty(test2)
+    expect(result).toBe(true)
+    expect(result2).toBe(false)
+  })
+
+  it('Should return the diference of transaction\'s time', () => {
+    const op1 = { transaction: { "merchant": "a", "amount": 180, "time": "2019-02-13T10:01:36.000Z" } }
+    const op2 = { transaction: { "merchant": "b", "amount": 100, "time": "2019-02-13T10:01:37.000Z" } }
+    const result = byDate(op1, op2)
+    expect(result).toBe(-1000)
+  })
 
   it('Should transform string lines into JSON object list', () => {
     const string = `{ "a": 1 }\n{ "b": 2 }\n{ "c": 3 }`
